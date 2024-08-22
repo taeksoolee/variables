bd301eec9c5329892d94a44b44e4f693674d555d
taeksoolee <leets1490@gmail.com>
2024-08-22 23:03:48 +0900

    First Commit

---
+++ variables/pages/error.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { NuxtError } from '#app';
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <h1>Not Found</h1>
+  <div>
+    <h1>{{ error?.statusCode }}</h1>
+  </div>
+</template>
---
+++ variables/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Index</h1>
+
+  <NuxtLink href="/about">About</NuxtLink> 
+</template>
